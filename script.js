67a696391af2bd50ae389f52037351277b725b00
mfranke84 <60253062+mfranke84@users.noreply.github.com>
2020-12-30 07:53:39 +0100

    Refactoring code

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toogle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
-const quoteGeneratorBtn = document.getElementById('quoteGenerator');
-const infinitYScrollBtn = document.getElementById('infinityScroll');
-const jokeTellerBtn = document.getElementById('jokeTeller');
+
 const urlQuoteGenerator = 'https://mfranke84.github.io/quote-generator/';
 const urlInfinityScroll = 'https://mfranke84.github.io/infinite-scroll/';
 const urlJokeTeller = 'https://mfranke84.github.io/joke-teller/';
@@ -19,59 +16,41 @@ const urlJokeTeller = 'https://mfranke84.github.io/joke-teller/';
 //     image3.src = `img/undraw_conceptual_idea_${theme}.svg`;
 // }
 
-// Dark Mode style
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
-    // modePictures('dark');
+// Toggle dark and light mode
+function setTheme(theme){
+    if (theme === 'light'){
+        isLight = true;
+    } else{
+        isLight = false;
+    }
+    nav.style.backgroundColor = isLight ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = isLight ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = isLight ? 'Light Mode': 'Dark Mode';
+    isLight ? toggleIcon.children[1].classList.replace('fa-moon','fa-sun') : toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
 }
-
-// Light Mode Style
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
-    // modePictures('light');
-}
-
 
 // Switch Theme Dynamically
 function switchTheme(event){
     if(event.target.checked){
-        document.documentElement.setAttribute('data-theme','dark');
-        localStorage.setItem('theme','dark');
-        darkMode();
+        document.documentElement.setAttribute('data-theme',DARK_THEME);
+        localStorage.setItem('theme',DARK_THEME);
+        setTheme(DARK_THEME);
     } else{
-        document.documentElement.setAttribute('data-theme','light');
-        localStorage.setItem('theme','light');
-        lightMode();
+        document.documentElement.setAttribute('data-theme',LIGHT_THEME);
+        localStorage.setItem('theme',LIGHT_THEME);
+        setTheme(LIGHT_THEME);
     }
 }
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
-// quoteGeneratorBtn.addEventListener('click', openProject(urlQuoteGenerator));
-quoteGeneratorBtn.addEventListener('click', ()=> {
-    window.open(urlQuoteGenerator);
-})
-infinitYScrollBtn.addEventListener('click', ()=> {
-    window.open(urlInfinityScroll);
-})
-jokeTellerBtn.addEventListener('click', ()=> {
-    window.open(urlJokeTeller);
-})
-
-
 
 // Load current theme from the storeage
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme){
     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark'){
+    if (currentTheme === DARK_THEME){
         toggleSwitch.checked = true;
-        darkMode();
+        setTheme(DARK_THEME);
     }
 }
